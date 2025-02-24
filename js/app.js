1a00b21c8fc784ac6a7f2ef513f1465675917b85
severogianluca <gianlucasevero300@gmail.com>
2025-02-24 18:16:30 +0100

    Feat: create logica sconto e calcolo del prezzo scontato

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 let eta = prompt('Inserisci la tua eta');
 let km = prompt('Inserisci quanti km vuoi percorrere');
+let prezzo = km *  (0.21);
+let sconto;
 
 
 eta = parseInt(eta);
@@ -9,5 +11,17 @@ km = parseInt(km);
 console.log(eta);
 console.log(km);
 
-let prezzo = km *  (0.21);
-console.log(prezzo)
+
+// logica per lo sconto
+if(eta < 18){
+    sconto = ((prezzo * 20) / 100);
+
+}else if(eta >= 65){
+    sconto = ((prezzo * 40) / 100);
+    
+}else{
+    sconto = 0;
+}
+
+prezzo = prezzo - sconto;
+console.log(prezzo);
